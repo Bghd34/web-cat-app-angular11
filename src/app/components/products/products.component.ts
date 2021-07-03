77dbbfe15cc710c8beff0e8ab434eb5246167444
Bghd34 <boughadiyacine@yahoo.fr>
2021-07-03 12:33:22 +0200

    modif

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product.model';
+import { ProductsService } from '../../services/products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products:Observable<Product[]> | null = null;
 
-  constructor() { }
+  constructor(private productService:ProductsService) { }
 
   ngOnInit(): void {
+  }
+
+  onGetAllProducts() {
+    this.products = this.productService.getAllProducts();
   }
 
 }
